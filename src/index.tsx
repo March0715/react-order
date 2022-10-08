38c3f5f55ef728ff57295c7c1055a8f81ddce9fa
machi <machi.wy@alibaba-inc.com>
2022-10-08 21:34:21 +0800

    feat:增加lint规则

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,4 @@ import ReactDOM from 'react-dom/client';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-  </React.StrictMode>
-);
+root.render(<React.StrictMode></React.StrictMode>);
